df9a8a50e0f30a2d99f6123236172c8c7db7f233
Bas Dijkstra <bas@ontestautomation.com>
2019-10-28 21:27:56 +0100

    Clean up

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -54,34 +54,4 @@ public class Car {
         List<String> italianCarsList = Arrays.asList("Fiat", "Alfa Romeo", "Lancia", "Maserati", "Ferrari");
         return italianCarsList.contains(this.make);
     }
-
-    public void anArrayOfCars() {
-
-        Car[] carArray = new Car[3];
-
-        carArray[0] = new Car("Fiat","Punto","red");
-        carArray[1] = new Car("Ford","Focus","blue");
-        carArray[2] = new Car("Maserati","Ghibli","black");
-
-        for(int count = 0; count < carArray.length; count++) {
-            System.out.println(
-                carArray[count].printInfo()
-            );
-        }
-    }
-
-    public void aListOfCars() {
-
-        List<Car> carList = new ArrayList<Car>();
-
-        carList.add(new Car("Fiat","Punto","red"));
-        carList.add(new Car("Ford","Focus","blue"));
-        carList.add(new Car("Maserati","Ghibli","black"));
-
-        for(Car car: carList) {
-            System.out.println(
-                car.printInfo()
-            );
-        }
-    }
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -3,22 +3,8 @@ import org.junit.Test;
 
 public class CarTest {
 
-    @Test
-    public void carArrayTest() {
-
-        Car myCar = new Car();
-        myCar.anArrayOfCars();
-    }
-
-    @Test
-    public void carListTest() {
-
-        Car myCar = new Car();
-        myCar.aListOfCars();
-    }
-
-    @Test
-    public void isItalianPositiveTest() {
+    @Test()
+    public void testPos() {
 
         // Arrange - Create a new instance of the Car class with prespecified property values
         Car myCar = new Car("Maserati","Ghibli","blue");
@@ -31,32 +17,15 @@ public class CarTest {
     }
 
     @Test
-    public void ifThenElseTest() {
+    public void testNeg() {
 
-        Car myCar = new Car("Fiat","Punto","red");
+        // Arrange - Create a new instance of the Car class with prespecified property values
+        Car myCar = new Car("Ford","Focus","black");
 
-        if(myCar.isItalian()) {
-            System.out.println("Buongiorno!");
-        }
-        else {
-            System.out.println("Hello!");
-        }
+        // Act - Invoke the method to be tested
+        boolean isItalian = myCar.isItalian();
+
+        // Assert - Check that the result of the method invocation matches the expected result
+        Assert.assertFalse(isItalian);
     }
-
-    private String getFruit() {
-
-        return "banana";
-    }
-
-    @Test
-    public void whileTest() {
-
-        int counter = 0;
-
-        while(counter < 5) {
-            System.out.println(String.format("And a %d...", counter));
-            counter++;
-        }
-    }
-
 }
